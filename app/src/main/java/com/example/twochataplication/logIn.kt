b76731cf8baa3eebd7f4336545f46df2e043cf26
taufik0910 <taufik.cradle@gmail.com>
2022-08-07 05:05:57 +0700

    create method direct sign up

--- app/src/main/java/com/example/twochataplication/logIn.kt
+++ app/src/main/java/com/example/twochataplication/logIn.kt
@@ -1,11 +1,28 @@
 package com.example.twochataplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class logIn : AppCompatActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btn_logIn)
+        btnSignup = findViewById(R.id.btn_signUp)
+
+        btnSignup.setOnClickListener{
+            val intent = Intent(this,signUp::class.java)
+            startActivity(intent)
+        }
     }
 }
