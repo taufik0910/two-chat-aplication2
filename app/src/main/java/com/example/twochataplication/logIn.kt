83018e74b0725d8d8d93823cbbd2be79d7152e3d
taufik0910 <taufik.cradle@gmail.com>
2022-08-07 07:21:14 +0700

    setting sampai firebase(waktu 1-11-00)

--- app/src/main/java/com/example/twochataplication/logIn.kt
+++ app/src/main/java/com/example/twochataplication/logIn.kt
@@ -5,24 +5,65 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class logIn : AppCompatActivity() {
+//    initial untuk view login
     private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignup: Button
+//    initial untuk firebase authentication
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+//
+//        hidden navbar login
+        supportActionBar?.hide()
 
+//        memanggil fungsi firebase
+        mAuth = FirebaseAuth.getInstance()
+//
+//        object untuk view
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btn_logIn)
         btnSignup = findViewById(R.id.btn_signUp)
 
+//    fungsi untuk direct ke form sign up
         btnSignup.setOnClickListener{
             val intent = Intent(this,signUp::class.java)
             startActivity(intent)
         }
+
+//        memanggil fungsi login
+        btnLogin.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+        Login(email,password);
+        }
+
     }
+//    membuat method login
+    private fun Login(email: String, password: String){
+//        logic login process
+    mAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Toast.makeText(this@logIn," Success Login", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@logIn, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@logIn,"some error accurate", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+
+}
 }
--- app/src/main/java/com/example/twochataplication/signUp.kt
+++ app/src/main/java/com/example/twochataplication/signUp.kt
@@ -1,11 +1,72 @@
 package com.example.twochataplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class signUp : AppCompatActivity() {
+    //    initial untuk view SignUp
+    private lateinit var edtName : EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignup: Button
+    //    initial untuk firebase authentication
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var mDbRef : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+//        hidden navbar login
+        supportActionBar?.hide()
+
+//        memanggil fungsi view
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignup = findViewById(R.id.btn_signUp)
+
+//        memanggil fungsi firebase
+        mAuth = FirebaseAuth.getInstance()
+
+        btnSignup.setOnClickListener {
+            val name = edtName.text.toString()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+            signup(name,email, password )
+        }
+    }
+//    membuat method signUp
+    private fun signup(name:String, email: String, password: String ){
+//        logic signUp
+    mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                addUserDataBase(name, email,mAuth.currentUser?.uid!!)
+                Toast.makeText(this@signUp,"Account Success Created", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@signUp, MainActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                // If sign in fails, display a message to the user.
+                Toast.makeText(this@signUp,"some error accurate", Toast.LENGTH_SHORT).show()
+
+
+            }
+        }
+    }
+
+    private fun addUserDataBase(name: String,email: String,uid:String){
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        mDbRef.child("user").child(uid).setValue(User(name, email, uid))
     }
 }
