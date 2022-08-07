35adbf746eb417a85d4923dbd970adfd6f038122
taufik0910 <taufik.cradle@gmail.com>
2022-08-08 01:36:04 +0700

    create visibility password

--- app/src/main/java/com/example/twochataplication/logIn.kt
+++ app/src/main/java/com/example/twochataplication/logIn.kt
@@ -3,10 +3,16 @@ package com.example.twochataplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import android.graphics.drawable.Drawable
+import android.text.method.PasswordTransformationMethod
+import android.widget.ImageView
+
 
 class logIn : AppCompatActivity() {
 //    initial untuk view login
@@ -14,12 +20,19 @@ class logIn : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignup: Button
+    private lateinit var vsb_password : ImageView
 //    initial untuk firebase authentication
     private lateinit var mAuth : FirebaseAuth
+
+    private var mIsShowPass = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
-//
+
+//        method show hidden password
+
+
 //        hidden navbar login
         supportActionBar?.hide()
 
@@ -31,6 +44,7 @@ class logIn : AppCompatActivity() {
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btn_logIn)
         btnSignup = findViewById(R.id.btn_signUp)
+        vsb_password = findViewById(R.id.vsb_password)
 
 //    fungsi untuk direct ke form sign up
         btnSignup.setOnClickListener{
@@ -46,8 +60,26 @@ class logIn : AppCompatActivity() {
         Login(email,password);
         }
 
+        vsb_password.setOnClickListener {
+            mIsShowPass = !mIsShowPass
+            showPassword(mIsShowPass)
+        }
+        showPassword(mIsShowPass)
     }
-//    membuat method login
+
+    private fun showPassword(isShow:Boolean){
+        if (isShow){
+        edtPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            vsb_password.setImageResource(R.drawable.ic_baseline_visibility_24)
+        }else{
+            edtPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            vsb_password.setImageResource(R.drawable.ic_baseline_visibility_off_24)
+        }
+        edtPassword.setSelection(edtPassword.text.toString().length)
+
+    }
+
+    //    membuat method login
     private fun Login(email: String, password: String){
 //        logic login process
     mAuth.signInWithEmailAndPassword(email, password)
