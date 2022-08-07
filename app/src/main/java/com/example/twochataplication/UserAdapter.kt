2ece8762a2819507e5648153543cd2cfee900c2e
taufik0910 <taufik.cradle@gmail.com>
2022-08-08 05:46:26 +0700

    create chat aktivity

--- app/src/main/java/com/example/twochataplication/UserAdapter.kt
+++ app/src/main/java/com/example/twochataplication/UserAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.twochataplication
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class UserAdapter(val context: Context, val userList: ArrayList<User>):
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -20,6 +22,13 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>):
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentUser = userList[position]
         holder.txtName.text = currentUser.name
+
+        holder.itemView.setOnClickListener{
+            val  intent = Intent(context,ChatActivity::class.java)
+            intent.putExtra("name",currentUser.name)
+            intent.putExtra("uid",currentUser.uid)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
